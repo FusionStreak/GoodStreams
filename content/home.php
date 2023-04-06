d11ebad685d312eadf842e5ea8586ed6cf5220e5
madelinequang9 <madelinequang9@gmail.com>
2023-04-06 09:59:07 -0400

    Search bar added

--- content/home.php
+++ content/home.php
@@ -1,1 +1,11 @@
-<h2> movies </h2>
+<h2> Search for movies to watch </h2>
+
+<div id="searchform">
+<form role="search">
+    <input type="search" id="query" name="s" placeholder="Search..." aria-label="Search movies">
+    <span id="search" class="material-symbols-outlined">search</span>
+</form>
+</div>
+
+<div id="content">
+</div>
