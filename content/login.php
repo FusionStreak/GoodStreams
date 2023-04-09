e8cf575edba69e7dba55822d28a444badab51705
Adegbesan Oreoluwa <oreoluwa626@gmail.com>
2023-04-09 03:37:00 -0400

    Updated css for login and registration pages

--- content/login.php
+++ content/login.php
@@ -16,9 +16,9 @@ require $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
     <p><?= $error ?></p>
   <?php endif; ?>
   <form action="login.php" method="post">
-    <label for="email">Email:</label>
+    <label for="email">Email</label>
     <input type="email" id="email" name="email"><br>
-    <label for="password">Password:</label>
+    <label for="password">Password</label>
     <input type="password" id="password" name="password"><br>
 
     <input type="submit" value="Login">
