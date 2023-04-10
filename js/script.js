ad9aae2dca9b27c763ca914661b267f6bf55b7b1
madelinequang9 <madelinequang9@gmail.com>
2023-04-10 18:42:48 -0400

    Responsive layout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 function openNav() {
-    document.getElementById("sideNav").style.width = "300px";
-    document.querySelector("body").style.marginLeft = "300px";
-    document.getElementById("close").style.display = "inline-block";
+    if (window.matchMedia("(max-width:500px").matches) {
+        document.getElementById("sideNav").style.width = "110px";
+        document.querySelector("body").style.marginLeft = "110px";
+        document.getElementById("close").style.display = "block";
+
+    }
+    else {
+        document.getElementById("sideNav").style.width = "300px";
+        document.querySelector("body").style.marginLeft = "300px";
+        document.getElementById("close").style.display = "inline-block";
+    }
     document.getElementById("menu").style.display = "none";
 }
 
@@ -9,6 +17,12 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.querySelector("body").style.marginLeft = "0";
     document.getElementById("close").style.display = "none";
-    document.getElementById("menu").style.display = "inline-block";
+    if (window.matchMedia("(max-width:500px").matches) {
+        document.getElementById("menu").style.display = "block";
+
+    }
+    else {
+        document.getElementById("menu").style.display = "inline-block";
+    }
 }
 
